43b2df6aaf7538d1782ad0ee313f0f570675392a
PasqualeP <90408829+pasqualepanico@users.noreply.github.com>
2022-08-10 19:37:51 +0200

    Random Advice App
    Deplyed with heroku 
    https://morning-hollows-69750.herokuapp.com/ | https://git.heroku.com/morning-hollows-69750.git

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+      res.sendFile(__dirname + "/index.html");
+
+});
+
+app.listen(process.env.PORT || 3000, function(req, res){
+  console.log("Server running on port 3000!");
+});
